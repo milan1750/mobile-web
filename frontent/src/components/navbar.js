68024a7607ac8587b5fc2fb47024c9ff0cd2a773
Milan Malla <milanmalla2015@gmail.com>
2024-01-16 20:56:20 +0000

    update

---
+++ frontent/src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+// We import bootstrap to make our application look better.
+import "bootstrap/dist/css/bootstrap.css";
+// We import NavLink to utilize the react router.
+import { NavLink } from "react-router-dom";
+// Here, we display our Navbar
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar  navbar-light bg-light p-4">
+        <NavLink className="navbar-brand" to="/">
+          <b style={{ width: 25 + "%" }}>KMS</b>
+        </NavLink>
+      </nav>
+    </div>
+  );
+}
