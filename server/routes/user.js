0cf4a7eee66f9d3c539aa3821cebc22ce5e1408b
Milan Malla <milanmalla2015@gmail.com>
2024-01-17 10:01:49 +0000

    update

--- server/routes/user.js
+++ server/routes/user.js
@@ -172,4 +172,99 @@ userRoutes.route("/project/:id").post(async function (req, res) {
   }
 });
 
+// Users
+
+// This section will help you delete a record
+userRoutes.route("/user/validate").post(async function (req, res) {
+  let db_connect = await dbo.getDb();
+  let myquery = { name: req.body.name };
+  try {
+    let user = await db_connect.collection("users").findOne(myquery);
+    console.log(user);
+    if (user) {
+      hash.comparePassword(
+        req.params.password,
+        user.password,
+        function (err, isPasswordMatch) {
+          console.log(isPasswordMatch);
+          if (isPasswordMatch) {
+            res.json(user);
+          } else {
+            res.json(user);
+            // res.status(401).send({ message: "Password is invalid" });
+          }
+        }
+      );
+    } else {
+      res.status(401).send({ message: "Username not found" });
+    }
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// This section will help you delete a record
+userRoutes.route("/user/create").post(async function (req, res) {
+  let db_connect = await dbo.getDb();
+  hash.cryptPassword(req.body.password, async function (err, pass) {
+    let myquery = {
+      name: req.body.name,
+      fullName: req.body.fullName,
+      email: req.body.email,
+      password: pass,
+    };
+    try {
+      let result = await db_connect.collection("users").insertOne(myquery);
+      res.json(result);
+    } catch (err) {
+      res.status(500).send("Internal Server Error");
+    }
+  });
+});
+
+// This section will help you delete a record
+userRoutes.route("/users").get(async function (req, res) {
+  let db_connect = await dbo.getDb();
+
+  try {
+    let result = await db_connect.collection("users").find({}).toArray();
+    res.json(result);
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// This section will help you get a single record by id
+userRoutes.route("/user/:id").get(async function (req, res) {
+	let db_connect = await dbo.getDb();
+	let myquery = { _id: new ObjectId(req.params.id) };
+	try {
+	  let result = await db_connect.collection("users").findOne(myquery);
+	  res.json(result);
+	} catch (err) {
+	  res.status(500).send("Internal Server Error");
+	}
+  });
+
+  // This section will help you create a new record.
+userRoutes.route("/user/:id").post(async function (req, res) {
+	// console.log(dbo);
+	let db_connect = dbo.getDb();
+	var myobj = {
+	  $set: { fullName: req.body.fullName, email: req.body.email },
+	};
+
+	let myquery = { _id: new ObjectId(req.params.id) };
+
+	try {
+	  let result = await db_connect
+		.collection("users")
+		.updateOne(myquery, myobj);
+	  res.json(result);
+	} catch (err) {
+	  console.log(err);
+	  res.status(500).send("Internal Server Error");
+	}
+  });
+
 module.exports = userRoutes;
